464c4f49d74c66901bcfc3924be451578f7be5cc
Henrique Tedeschi <htedeschi@Melaleuca.com>
2019-04-24 12:02:31 -0600

    Changing hook to work with new structs

--- resources/google/hook.js
+++ resources/google/hook.js
@@ -1,3 +1,7 @@
+var Text = require('./struct').Text;
+var ImageObj = require('./struct').ImageObj;
+var ResponseObject = require('./struct').ResponseObject;
+
 const TUITION_NON_LDS = 4118;
 const TUITION_LDS = 2059;
 
@@ -10,59 +14,24 @@ exports.getAppropriateAnswer = function (incomingIntent, callback = undefined) {
 
     if (incomingIntent.parameters.intent == 'tuition-cost') {
 
-        response = {};
-
         response_general = `Tuition cost \$${TUITION_NON_LDS} for non-members of the church of Jesus Christ, and \$${TUITION_LDS} per semester for members.`;
         response_lds = `The cost of tuition is \$${TUITION_LDS} per semester for members of the church of Jesus Christ.`;
         response_non_lds = `The cost of tuition is \$${TUITION_NON_LDS} for non-members of the church of Jesus Christ`;
 
-        response.text = `Tuition cost \$${TUITION_NON_LDS} for non-members of the church of Jesus Christ, and \$${TUITION_LDS} per semester for members.`;
-        response.reader = (incomingIntent.parameters.lds_membership ? (incomingIntent.parameters.lds_membership == 'membership.non-lds' ? response_non_lds : response_lds) : response_general);
+        response = (incomingIntent.parameters.lds_membership ? (incomingIntent.parameters.lds_membership == 'membership.non-lds' ? response_non_lds : response_lds) : response_general);
 
-        // responseObj = objectResponse(response);
+        mytext = new Text([
+            response
+        ]);
+
+        myimage = new ImageObj("http://www.byui.edu/images/home_page/Ready%20Round%207/7-Welding-Real-World%20HOME.jpg",
+            "A student welding, working on a specialized skill with the caption \"Ready for real - world Preparation.\"");
+
+        responseObj = new ResponseObject(mytext.text[0], textObject = mytext, imageObject = myimage);
     }
     if (callback) {
         callback(responseObj);
     } else {
         return responseObj;
     }
-}
-
-function objectResponse(response) {
-    if (!response.text) {
-        return false;
-    }
-    responseObj = {};
-    responseObj.fulfillmentText = response.text;
-    responseObj.fulfillmentMessages = [];
-
-    responseObj = {
-        "fulfillmentText": response.text,
-        "fulfillmentMessages": [
-            {
-                "text": {
-                    "text": [response.reader]
-                }
-            },
-            {
-                "image": {
-                    "imageUri": "http://www.byui.edu/a/80408",
-                    "accessibilityText": ""
-                }
-            },
-            {
-                "quickReplies": {
-                    "title": "Suggested replies",
-                    "quickReplies": [
-                        "See more details",
-                        "Call financial aid",
-                        "Cancel"
-                    ]
-                }
-            }
-        ],
-        "source": "action-skill-byui"
-    }
-
-    return responseObj;
 }
